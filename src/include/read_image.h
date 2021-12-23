1b20d1de6323dc4faf4afdaab70480bc4c85e462
kgandhi09 <kushal009gandhi@gmail.com>
2021-12-23 14:10:17 -0500

    started with the image reader class

---
+++ src/include/read_image.h
@@ -0,0 +1,15 @@
+/*
+ * read_image.h
+ *
+ *  Created on: Dec 23, 2021
+ *      Author: kgandhi
+ */
+
+#ifndef INCLUDE_READ_IMAGE_H_
+#define INCLUDE_READ_IMAGE_H_
+
+
+
+
+
+#endif /* INCLUDE_READ_IMAGE_H_ */
