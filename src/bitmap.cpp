c3af94109168e2caa9193c935ef6f2b5c0e73b7b
kgandhi09 <kushal009gandhi@gmail.com>
2021-12-27 17:07:15 -0500

    bmp image construction learning in progress

--- src/bitmap.cpp
+++ src/bitmap.cpp
@@ -44,6 +44,52 @@ void bitmap_image::write_bitmap_image(string write_path){
 }
 
 Color bitmap_image::getColor(int x, int y){
+	return bitmap_image::m_colors[y*bitmap_image::m_width + x];
+}
+
+void bitmap_image::setColor(Color& color, int x, int y){
+	bitmap_image::m_colors[y*bitmap_image::m_width+x].r = color.r;
+	bitmap_image::m_colors[y*bitmap_image::m_width+x].g = color.g;
+	bitmap_image::m_colors[y*bitmap_image::m_width+x].b = color.b;
+}
+
+void bitmap_image::write_bitmap_image(string write_path){
+
+	ofstream f;
+	f.open(write_path, ios::out | ios::binary);
+
+	if(!f.is_open()){
+		cerr << "Could not open the file, please check if the path is correct!" << endl;
+		return;
+	}
+
+	//Three parts required to write a bitmap image
+	//File Header - General information
+	//Information header (DIB header) - Detailed information
+	//Pixel array - Color data for each pixel
+
+	char bmpPadding[3] = {0,0,0};
+	int paddingAmount = (bitmap_image::m_width*4 - bitmap_image::m_width*3) % 3;
+
+	int fileHeaderSize = 14; //file header size will always be 14 bytes for a bitmap image
+	int infoHeaderSize = 40; //info header size will always be 40 bytes for a bitmap image
+	int fileSize = fileHeaderSize + infoHeaderSize + (bitmap_image::m_width * bitmap_image::m_height * 3) + (paddingAmount * bitmap_image::m_height);
+
+	char fileHeader[fileHeaderSize];
+	//File Type
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
